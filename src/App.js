3c3bef25db43b317c52d0c74a0e92264add483fd
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-11-23 04:44:43 -0500

    Added protected routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './styles/App.css'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { UserContext } from './contexts/UserContext'
 import NavBar from './components/NavBar'
 import Home from './components/routes/Home'
 import Login from './components/routes/Login'
@@ -7,20 +9,29 @@ import Register from './components/routes/Register'
 import ProfileForm from './components/routes/ProfileForm'
 
 const App = () => {
+  const { authenticated } = useContext(UserContext)
+
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {authenticated && (
+        <header>
+          <NavBar />
+        </header>
+      )}
 
       <h1>Flixder</h1>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profiles/new" element={<ProfileForm />} />
-      </Routes>
+      {authenticated ? (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profiles/new" element={<ProfileForm />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Login />} />
+        </Routes>
+      )}
     </div>
   )
 }
