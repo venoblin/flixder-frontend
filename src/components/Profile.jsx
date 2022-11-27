21f5092f0c099a6ffce6d258e3de2351169239e1
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-11-27 03:16:53 -0500

    Added Profile styles and a link

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import '../styles/Profile.css'
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { UserContext } from '../contexts/UserContext'
 import { TMDB_IMG_BASE } from '../global'
 
@@ -9,40 +10,46 @@ const Profile = () => {
   return (
     <div className="Profile">
       <div className="profile-card">
-        <div className="left-wrapper">
-          <img
-            className="profile-pic"
-            src={currentProfile.profile_pic.url}
-            alt={`${currentProfile.name} ${currentProfile.profile_pic.name}`}
-          />
+        <div className="profile-info">
+          <div className="left-wrapper">
+            <img
+              className="profile-pic"
+              src={currentProfile.profile_pic.url}
+              alt={`${currentProfile.name} ${currentProfile.profile_pic.name}`}
+            />
 
-          <h2>{currentProfile.name}</h2>
+            <h2>{currentProfile.name}</h2>
 
-          <img
-            className="flag"
-            src={currentProfile.region.flag_image}
-            alt={`${currentProfile.region.name} flag`}
-          />
-        </div>
-
-        <div className="right-wrapper">
-          <div className="genres">
-            {currentProfile.fav_genres.map((genre) => (
-              <p key={genre._id}>{genre.name}</p>
-            ))}
+            <img
+              className="flag"
+              src={currentProfile.region.flag_image}
+              alt={`${currentProfile.region.name} flag`}
+            />
           </div>
 
-          <div className="providers">
-            {currentProfile.providers.map((provider) => (
-              <img
-                key={provider._id}
-                className="provider-logo"
-                src={`${TMDB_IMG_BASE}${provider.logo_path}`}
-                alt={`${provider.provider_name} logo`}
-              />
-            ))}
+          <div className="right-wrapper">
+            <div className="genres">
+              {currentProfile.fav_genres.map((genre) => (
+                <p key={genre._id}>{genre.name}</p>
+              ))}
+            </div>
+
+            <div className="providers">
+              {currentProfile.providers.map((provider) => (
+                <img
+                  key={provider._id}
+                  className="provider-logo"
+                  src={`${TMDB_IMG_BASE}${provider.logo_path}`}
+                  alt={`${provider.provider_name} logo`}
+                />
+              ))}
+            </div>
           </div>
         </div>
+
+        <Link className="btn" to="/find">
+          Find New Movies!
+        </Link>
       </div>
 
       <div className="movies">
