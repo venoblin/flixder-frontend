20e66bea43e81ffcf1ff3872287f9104141ea8dd
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-11-27 03:10:36 -0500

    Added Profile styles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,35 +8,41 @@ const Profile = () => {
 
   return (
     <div className="Profile">
-      <img
-        className="profile-pic"
-        src={currentProfile.profile_pic.url}
-        alt={`${currentProfile.name} ${currentProfile.profile_pic.name}`}
-      />
-
-      <h2>{currentProfile.name}</h2>
-
-      <img
-        className="flag"
-        src={currentProfile.region.flag_image}
-        alt={`${currentProfile.region.name} flag`}
-      />
-
-      <div className="providers">
-        {currentProfile.providers.map((provider) => (
+      <div className="profile-card">
+        <div className="left-wrapper">
           <img
-            key={provider._id}
-            className="provider-logo"
-            src={`${TMDB_IMG_BASE}${provider.logo_path}`}
-            alt={`${provider.provider_name} logo`}
+            className="profile-pic"
+            src={currentProfile.profile_pic.url}
+            alt={`${currentProfile.name} ${currentProfile.profile_pic.name}`}
           />
-        ))}
-      </div>
 
-      <div className="genres">
-        {currentProfile.fav_genres.map((genre) => (
-          <p key={genre._id}>{genre.name}</p>
-        ))}
+          <h2>{currentProfile.name}</h2>
+
+          <img
+            className="flag"
+            src={currentProfile.region.flag_image}
+            alt={`${currentProfile.region.name} flag`}
+          />
+        </div>
+
+        <div className="right-wrapper">
+          <div className="genres">
+            {currentProfile.fav_genres.map((genre) => (
+              <p key={genre._id}>{genre.name}</p>
+            ))}
+          </div>
+
+          <div className="providers">
+            {currentProfile.providers.map((provider) => (
+              <img
+                key={provider._id}
+                className="provider-logo"
+                src={`${TMDB_IMG_BASE}${provider.logo_path}`}
+                alt={`${provider.provider_name} logo`}
+              />
+            ))}
+          </div>
+        </div>
       </div>
 
       <div className="movies">
