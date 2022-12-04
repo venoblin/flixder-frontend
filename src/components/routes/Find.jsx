84cc70edb07f5f448eea6b73a6292debfb8342cf
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-12-03 22:43:02 -0500

    Fixed bug

--- src/components/routes/Find.jsx
+++ src/components/routes/Find.jsx
@@ -6,7 +6,7 @@ import MovieStack from '../MovieStack'
 
 const Find = () => {
   const { currentProfile } = useContext(UserContext)
-  const [movies, setMovies] = useState(null)
+  const [movies, setMovies] = useState([])
 
   const getMovies = async () => {
     const res = await GetTmdbMovies(currentProfile)
@@ -21,7 +21,7 @@ const Find = () => {
     <div className="Find">
       <h1>Find</h1>
 
-      {movies && <MovieStack movies={movies} findMode={true} />}
+      {movies.length && <MovieStack movies={movies} findMode={true} />}
     </div>
   )
 }
